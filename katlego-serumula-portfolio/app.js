5bedafab9efa96ee194e0142e9c49e1e91732777
KATLEGO SERUMULA <87599678+katlegoserumula4@users.noreply.github.com>
2023-10-11 23:37:47 +0200

    Add files via upload

---
+++ katlego-serumula-portfolio/app.js
@@ -0,0 +1,36 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+
+function openDocumentInNewTab(url) {
+    window.open(url, '_blank');
+}
+
+// Example usage:
+const documentURL = '.docs/CV-Katlego-Serumula.pdf';
+openDocumentInNewTab(documentURL);
+
+
